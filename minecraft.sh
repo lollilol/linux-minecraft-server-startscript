b7d71387b683ed18c6df8b65f841e9be19ba1785
lollilol <lollilol@doorcraft.de>
2018-11-07 19:19:35 +0000

    major update
    changelog:
    - added server kill feature
    - removed warning message when opening console
    - added warning if server is already running/already stopped.

--- minecraft.sh
+++ minecraft.sh
@@ -2,7 +2,7 @@
 ##configuration
 
 #minecraft server .jar Name
-binary=minecraft_server.1.12.2.jar
+binary=minecraft_server.1.13.2.jar
 
 #directory of the minecraft server, write "." if the startscript is in the same folder as the minecraft server.
 directory=/home/minecraft
@@ -26,31 +26,49 @@ ram=1G
 script=`basename "$0"`
 case "$1" in
 start)
-	cd $directory
-	screen -AmdS $name java -Xms$ram -Xmx$ram -jar $binary nogui
-	sleep 1
-	echo "\033[32m$name-server was started..\033[0m"
+	if ! screen -ls | grep -q "$name"; then
+		cd $directory
+		screen -AmdS $name java -Xms100M -Xmx$ram -jar $binary nogui
+		sleep 1
+		echo "\033[32m$name-server was started..\033[0m"
+	else
+		echo "\033[31m$name-server is already running!\033[0m"
+	fi
 	;;
 stop)
-  screen -S $name -p 0 -X stuff "kick @a The server is restarting or got stopped.^M"
-	screen -S $name -p 0 -X stuff "stop^M"
-	sleep 1
-	echo "\033[31m$name-server was stopped..\033[0m"
+	if screen -ls | grep -q "$name"; then
+		screen -S $name -p 0 -X stuff "kick @a The server is restarting or got stopped.^M"
+		screen -S $name -p 0 -X stuff "^C^M"
+		sleep 1
+		echo "\033[31m$name-server was stopped..\033[0m"
+	else
+		echo "\033[31m$name-server is not running!\033[0m"
+	fi
 	;;
 restart)
 	$0 stop
 	$0 start
 	;;
+kill)
+	if screen -ls | grep -q "$name"; then
+		screen -XS $name quit
+		echo "\033[31m$name-server was killed..\033[0m"
+	else
+		echo "\033[31m$name-server is not running!\033[0m"
+	fi
+	;;
 reload)
   screen -S $name -p 0 -X stuff "reload^M"
 	;;
 console)
-  echo "\033[32mTo exit the console without stopping the server, press "CTRL+A+D"\033[0m"
-	sleep 3
-	screen -r $name
+	if screen -ls | grep -q "$name"; then
+		screen -r $name
+	else
+		echo "\033[31m$name-server is not running!\033[0m"
+	fi
 	;;
 *)
-	echo "\033[31mUsage: './$script (start|stop|restart|reload|console)'\033[0m"
+	echo "\033[31mUsage: './$script (start|stop|restart|kill|reload|console)'\033[0m"
 	;;
 esac
 exit 0

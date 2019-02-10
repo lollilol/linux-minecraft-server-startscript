6c70c6665335638d6182d4bd6247a5383dddfa90
lollilol <38194372+lollilol@users.noreply.github.com>
2019-02-10 22:36:39 +0100

    major changes
    changed to other default values
    added backup and world-save function
    if running any other than paper and want to reload with the script, you have to change the line 88 to ... "reload^M"
    updated usage

--- minecraft.sh
+++ minecraft.sh
@@ -6,10 +6,10 @@ binary=paper.jar
 
 #directory of the minecraft server, write "." if the startscript is in the same folder as the minecraft server.
 #dont put a "/" at the end of the line! otherwise update wont work!
-directory=/usr/minecraft
+directory=/home/minecraft/survival
 
 #name, used as screen and display-name in the entire script"
-name=minecraft
+name=survival
 
 ##RAM
 # usage
@@ -18,11 +18,16 @@ name=minecraft
 # for 512 Megabyte, write: "512M"
 # for 256 Megabyte, write: "256M"
 # etc..
-ram=1G
+ram=4G
 
 ##Updating
 #Here you can specify a url to an always up-to-date server.jar
 url=https://papermc.io/ci/job/Paper-1.13/lastSuccessfulBuild/artifact/paperclip.jar
+
+##Backup
+backup_folder=backups
+backup_file=world-$(date +"%d-%m-%y_%H:%M").tar.gz
+
 
 ##########################################
 # no neccesary changes beyond this line! #
@@ -52,6 +57,7 @@ stop)
 	;;
 restart)
 	$0 stop
+	sleep 5
 	$0 start
 	;;
 status)
@@ -59,6 +65,14 @@ status)
 		echo "\033[32m$name-server is running.\033[0m"
 	else
 		echo "\033[31m$name-server is not running.\033[0m"
+	fi
+	;;
+save)
+	if screen -ls | grep -q "$name"; then
+		screen -S $name -p 0 -X stuff "save-all^M"
+		echo "\033[32m$name-server was saved..\033[0m"
+	else
+		echo "\033[31m$name-server is not running!\033[0m"
 	fi
 	;;
 kill)
@@ -71,7 +85,7 @@ kill)
 	;;
 reload)
 	if screen -ls | grep -q "$name"; then
-		screen -S $name -p 0 -X stuff "reload^M"
+		screen -S $name -p 0 -X stuff "reload confirm^M"
 		echo "\033[32m$name-server was reloaded..\033[0m"
 	else
 		echo "\033[31m$name-server is not running!\033[0m"
@@ -87,8 +101,29 @@ console)
 update)
 	wget -O $directory/$binary $url
 	;;
+backup)
+	if [ -d "$backup_folder" ]
+	then
+		$0 save
+		sleep 2 #if the server tooks longer saving the world than these two seconds, the backup file will be corrupted. So be sure this is set right.
+		cd $directory
+		echo "\033[32mStarting Backup progress...\033[0m"
+		tar -czf $backup_folder/$backup_file world/
+		echo "\033[32mWorld successfully backupped. Location: $backup_folder/$backup_file\033[0m"
+	else
+		$0 save
+		sleep 2
+		cd $directory
+		echo "\033[31mBackup Folder doesnt exist. Creating it...\033[0m"
+		sleep 1
+		mkdir $backup_folder
+		echo "\033[32mStarting Backup progress...\033[0m"
+		tar -czf $backup_folder/$backup_file world/
+		echo "\033[32mWorld successfully backupped. Location: $backup_folder/$backup_file\033[0m"
+	fi
+	;;
 *)
-	echo "\033[31mUsage: '$0 (start|stop|restart|status|kill|reload|console|update)'\033[0m"
+	echo "\033[31mUsage: '$0 (start|stop|restart|status|save|kill|reload|console|update|backup)'\033[0m"
 	;;
 esac
 exit 0
